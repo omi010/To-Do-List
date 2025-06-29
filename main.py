baa7295e4a25be006f72b2da49291bdd9d34ac4b
omi010 <omkarmali699@yahoo.in>
2025-06-29 21:24:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+todos = []
+
+while True:
+    user_action = input("type add, show, edit, complete or exit: ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo:")
+            todos.append(todo)
+        case 'show':
+            for index, item in enumerate(todos):
+                row = f"{index + 1}-{item}"
+                print(row)
+        case 'edit':
+            number = int(input("Number of the todo to edit: "))
+            number = number - 1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
+        case 'complete':
+            number = int(input("Number of the todo to complete: "))
+            todos.pop(number - 1)
+        case 'exit':
+            break
+        #case Whatever:
+         #   print("Hey, you entered an unkown command")
+
+print("Bye!")
